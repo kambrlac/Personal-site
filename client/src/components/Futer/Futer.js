542bc92b29eb554dfe13cfc7401774b680db97ca
kambrlac <87143957+kambrlac@users.noreply.github.com>
2022-03-01 00:52:46 +0100

    gon

--- client/src/components/Futer/Futer.js
+++ client/src/components/Futer/Futer.js
@@ -8,11 +8,11 @@ function Futer(){
                 <div className='media'>
                         <p>Follow me</p>
                         <a className='ico' href='https://www.instagram.com/branko_petrovic/?hl=en'><i class="fab fa-instagram fa-2x"></i></a>
-                        <a className='ico' href='/long-gone'><i class="fab fa-facebook fa-2x"></i></a>
+                        <a className='ico' href='/gone'><i class="fab fa-facebook fa-2x"></i></a>
                 </div>
                 <div className='contact'>
                         <p>Contact me</p>
-                        <a className='ico' href='/'><i class="far fa-envelope fa-2x"></i></a>
+                        <a className='ico' href='/comment'><i class="far fa-envelope fa-2x"></i></a>
                         <a className='ico' href='tel:+381649827907'><i class="fas fa-phone-alt fa-2x"></i></a>
                 </div>
         </div>       
